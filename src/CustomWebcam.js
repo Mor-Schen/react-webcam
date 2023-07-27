62e5113c0259c103c2fa89eb815e18824fbb455a
Mor Schenavsky <m.schenavsky@gmail.com>
2023-07-27 12:53:20 +0300

    react webcam new

---
+++ src/CustomWebcam.js
@@ -0,0 +1,16 @@
+import Webcam from "react-webcam";
+import { useRef } from "react"; // import useRef
+
+
+const CustomWebcam = () => {
+    const webcamRef = useRef(null); // create a webcam reference
+
+    return (
+        <div className="container">
+        <Webcam height={600} width={600} />
+        </div>
+    );
+};
+
+export default CustomWebcam;
+
