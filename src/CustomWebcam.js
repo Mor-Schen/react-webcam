aad41558eb5b9108da794644986776d76d74cce4
Mor Schenavsky <m.schenavsky@gmail.com>
2023-08-02 16:07:01 +0300

    add styling

--- src/CustomWebcam.js
+++ src/CustomWebcam.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Webcam from "react-webcam";
-import { useRef } from "react"; 
+//import { useRef } from "react"; 
+import useStyles from "./CustomWebcamStyles"; // Import the styles from the separate file
 
 
 const CustomWebcam = () => {
-    const webcamRef = useRef(null); // create a webcam reference
+    const classes = useStyles();
+    //const webcamRef = useRef(null); // create a webcam reference
 
     return (
-        <div className="container">
-            <Webcam height={600} width={600} />
+        <div className={classes.container}>
+            <div className={classes.webcam}>
+                <Webcam height={600} width={600} />
+            </div>
         </div>
     );
 };
 
 export default CustomWebcam;
+
