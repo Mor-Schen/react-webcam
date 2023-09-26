524f652ece498d8b5344fa8cdc1b88fcf8751ebd
Mor-Schen <m.schenavsky@gmail.com>
2023-09-26 23:06:53 +0300

    Update CustomWebcam.js
    Add video recording feature to CustomWebcam.js

--- src/CustomWebcam.js
+++ src/CustomWebcam.js
@@ -1,13 +1,63 @@
 import React from "react";
 import Webcam from "react-webcam";
-import { useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 
 const CustomWebcam = () => {
   const webcamRef = useRef(null);
+  const mediaRecorderRef = useRef(null);
+  const [capturing, setCapturing] = useState(false);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+  const [videoURL, setVideoURL] = useState(null);
+
+  const handleStartCaptureClick = () => {
+    setRecordedChunks([]);
+    const stream = webcamRef.current.video.srcObject;
+    mediaRecorderRef.current = new MediaRecorder(stream, {
+      mimeType: "video/webm",
+    });
+    mediaRecorderRef.current.ondataavailable = (e) => {
+      if (e.data.size > 0) {
+        setRecordedChunks((prev) => [...prev, e.data]);
+      }
+    };
+    mediaRecorderRef.current.onstop = () => {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+      const url = URL.createObjectURL(blob);
+      setVideoURL(url);
+    };
+    mediaRecorderRef.current.start();
+    setCapturing(true);
+  };
+
+  const handleStopCaptureClick = () => {
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+  };
+
+  useEffect(() => {
+    if (videoURL) {
+      // You can now do something with the recorded video, like sending it to ChatGPT for text conversion.
+      console.log("Video URL:", videoURL);
+    }
+  }, [videoURL]);
 
   return (
     <div className="container">
-      <Webcam height={600} width={600} />
+      <Webcam ref={webcamRef} height={600} width={600} />
+      <div>
+        {capturing ? (
+          <button onClick={handleStopCaptureClick}>Stop Capture</button>
+        ) : (
+          <button onClick={handleStartCaptureClick}>Start Capture</button>
+        )}
+      </div>
+      {videoURL && (
+        <div>
+          <video controls autoPlay src={videoURL} width={600} height={400} />
+        </div>
+      )}
     </div>
   );
 };
